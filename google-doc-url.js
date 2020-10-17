0345e031835888ac4703cc2cb60c3168f49cf126
alexawl <51390045+alexawl@users.noreply.github.com>
2020-10-17 18:46:13 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Lr-ke_7ar-11PmExy2h-wQzTG-kU2ue3mC58izYwzg0/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1cOqpryRAit3PO6GmZSieiLj-pqED6wguzJDSb7X922w/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
